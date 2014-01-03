809d97714992df2d6b1fad743c8ae5461ab19fd0
Jeremy Thurgood <firxen@gmail.com>
2014-01-03 13:57:35 +0200

    Packaging and CI.

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os.path
+
+from setuptools import setup, find_packages
+
+
+def readme():
+    path = os.path.join(os.path.dirname(__file__), 'README.md')
+    return open(path, 'r').read()
+
+
+setup(
+    name="txTwitter",
+    version="0.1.0a",
+    url='https://github.com/jerith/txTwitter',
+    license='MIT',
+    description="A Twisted-based client library for Twitter's API.",
+    long_description=readme(),
+    author='Jeremy Thurgood',
+    author_email='firxen@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['Twisted', 'oauth2', 'pyOpenSSL'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Twisted',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
