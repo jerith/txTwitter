1b3221e30f30044f9079b10a63b285b52887394e
Jeremy Thurgood <firxen@gmail.com>
2014-10-06 13:51:05 +0200

    Bump version post-release.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def readme():
 
 setup(
     name="txTwitter",
-    version="0.1.5",
+    version="0.1.6a",
     url='https://github.com/jerith/txTwitter',
     license='MIT',
     description="A Twisted-based client library for Twitter's API.",
